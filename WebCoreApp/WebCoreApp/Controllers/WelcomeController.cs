61c953e0987aede68d4564349d95b239064fde53
dacthem <121852156+dacthem@users.noreply.github.com>
2023-01-12 08:22:09 +0700

    up

---
+++ WebCoreApp/WebCoreApp/Controllers/WelcomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebCoreApp.Models;
+using WebCoreApp.Services;
+
+namespace WebCoreApp.Controllers
+{
+    public class WelcomeController : Controller
+    {
+		private readonly IProductService _productService;
+        public WelcomeController(IProductService productService)
+        {
+            _productService = productService;
+        }
+		public IActionResult Index()
+        {
+			var model = new ProductViewModel();
+			model.Products = _productService.GetList();
+			return View(model);
+        }
+        
+    }
+}
